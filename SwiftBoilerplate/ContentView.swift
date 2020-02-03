bd773557b8639a803aff1da02763579f3d70f4d1
Masanao Imai <masa7351@gmail.com>
2020-02-03 20:20:36 +0900

    initial commit

---
+++ SwiftBoilerplate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftBoilerplate
+//
+//  Created by Masanao Imai on 2020/02/03.
+//  Copyright © 2020 Masanao Imai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
